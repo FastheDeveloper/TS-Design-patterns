b3b1e45cdcd2c38296d67d8faab90629a5453c4f
Farouq Seriki <farouq@basaltx.io>
2024-07-20 16:56:00 +0100

    Encapsulation

--- excersises/abstractionIntro.ts
+++ excersises/abstractionIntro.ts
@@ -1,49 +1,75 @@
 // //shapes
-// //Calculates Area And Perimeter
-// //Simple-single function calculateTotalArea
+//Calculates Area And Perimeter
+//Simple-single function calculateTotalArea
 
-// //Interface of Shape
+//Interface of Shape
 
-// //Date Class
-// //Get Current Year
-// //Get Current Month
-// //Get Current Date
+//Date Class
+//Get Current Year
+//Get Current Month
+//Get Current Date
 
-// interface Shape {
-//   area(): number;
-//   perimeter(): number;
-// }
+const now = new Date();
 
-// class Circle implements Shape {
-//   constructor(private radius: number) {}
-//   area(): number {
-//     return Math.PI * this.radius * this.radius;
-//   }
+const currentYear = now.getFullYear();
+const currentMonth = now.getMonth() + 1;
+const currentDate = now.getDate();
 
-//   perimeter(): number {
-//     return 2 * Math.PI * this.radius;
-//   }
-// }
+console.log(currentYear);
+console.log(currentMonth);
+console.log(currentDate);
 
-// class Rectangle implements Shape {
-//   constructor(
-//     private width: number,
-//     private height: number,
-//   ) {}
-//   area(): number {
-//     return this.width * this.height;
-//   }
+//shapes
+//Calculates Area And Perimeter
+//Simple-single function calculateTotalArea
 
-//   perimeter(): number {
-//     return 2 * (this.width + this.height);
-//   }
-// }
+//Interface of Shape
 
-// function calculateTotalArea(Shape: Shape): number {
-//   return Shape.area();
-// }
+//Date Class
+//Get Current Year
+//Get Current Month
+//Get Current Date
 
-// let circle: Circle = new Circle(5);
-// let rectangle: Rectangle = new Rectangle(4, 6);
-// console.log("Area of circle", calculateTotalArea(circle));
-// console.log("Area of rectangle", calculateTotalArea(rectangle));
+interface Shape {
+  area(): number;
+  perimeter(): number;
+}
+
+class Circle implements Shape {
+  constructor(private radius: number) {}
+  area(): number {
+    return Math.PI * this.radius * this.radius;
+  }
+
+  perimeter(): number {
+    return 2 * Math.PI * this.radius;
+  }
+}
+
+class Rectangle implements Shape {
+  constructor(
+    private width: number,
+    private height: number,
+  ) {}
+  area(): number {
+    return this.width * this.height;
+  }
+
+  perimeter(): number {
+    return 2 * (this.width + this.height);
+  }
+}
+
+function calculateTotalArea(Shape: Shape): number {
+  return Shape.area();
+}
+
+function calculatePerimeter(Shape: Shape): number {
+  return Shape.perimeter();
+}
+let circle: Circle = new Circle(5);
+let rectangle: Rectangle = new Rectangle(4, 6);
+console.log("Area of circle", calculateTotalArea(circle));
+console.log("Area of rectangle", calculateTotalArea(rectangle));
+console.log("Perimeter of circle", calculatePerimeter(circle));
+console.log("Perimeter of rectangle", calculatePerimeter(rectangle));
