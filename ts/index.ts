8aac5e5b4864e7ebe1ac357fac5c06a1b7c2ceff
Farouq Seriki <farouq@basaltx.io>
2024-07-21 22:04:12 +0100

    Inheritance

--- ts/index.ts
+++ ts/index.ts
@@ -1,52 +1,56 @@
+// Polymorphisim ↓ ↓ ↓ ↓ ↓ ↓
+
+// Polymorphisim↑↑↑↑↑↑↑↑↑
+
 // Encapsulation↓ ↓ ↓ ↓ ↓ ↓ ↓
 //BankAccount
 //Depositing
 //Withdrawing
 //Balance
 
-class BankAccount {
-  private _balance: number;
-  constructor(initialBalance: number) {
-    this._balance = initialBalance;
-  }
+// class BankAccount {
+//   private _balance: number;
+//   constructor(initialBalance: number) {
+//     this._balance = initialBalance;
+//   }
 
-  //getter to return balance
-  public get balance(): number {
-    return this._balance;
-  }
+//   //getter to return balance
+//   public get balance(): number {
+//     return this._balance;
+//   }
 
-  //Deposit Method
-  public deposit(amount: number): void {
-    if (amount < 0) {
-      console.log("Invalid");
-      return;
-    }
-    this._balance += amount;
-  }
+//   //Deposit Method
+//   public deposit(amount: number): void {
+//     if (amount < 0) {
+//       console.log("Invalid");
+//       return;
+//     }
+//     this._balance += amount;
+//   }
 
-  //withdraw method
-  public withdraw(amount: number): void {
-    if (amount < 0) {
-      console.log("Invalid");
-      return;
-    }
+//   //withdraw method
+//   public withdraw(amount: number): void {
+//     if (amount < 0) {
+//       console.log("Invalid");
+//       return;
+//     }
 
-    if (this._balance < amount) {
-      console.log("Invalid");
-      return;
-    }
+//     if (this._balance < amount) {
+//       console.log("Invalid");
+//       return;
+//     }
 
-    this._balance -= amount;
-  }
-}
+//     this._balance -= amount;
+//   }
+// }
 
-let FasAccount = new BankAccount(500);
+// let FasAccount = new BankAccount(500);
 
-console.log(FasAccount.balance);
-FasAccount.deposit(500);
-console.log(FasAccount.balance);
-FasAccount.withdraw(200);
-console.log(FasAccount.balance);
+// console.log(FasAccount.balance);
+// FasAccount.deposit(500);
+// console.log(FasAccount.balance);
+// FasAccount.withdraw(200);
+// console.log(FasAccount.balance);
 
 // Encapsulation↑↑↑↑↑↑↑↑↑
 
