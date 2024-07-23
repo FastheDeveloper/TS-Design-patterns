4626215ce1c4ebcb7c90e803899d6b6fa048fca9
Farouq Seriki <farouq@basaltx.io>
2024-07-23 11:32:55 +0100

    Liskov Payment Processor

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,90 @@
 //S - SINGLE RESPONSIBILITY PRINCIPLE ( A CLASS SHOULD ONLY DO ONE THING)
 //O - OPEN CLOSE PRINCIPLE (A CLASS SHOULD BE OPEN TO EXTENSION BUT CLOSED TO MODIFICATION)
+//L -  LISKOV SUBSTITITION PRINCIPLE ( IF S IS A SUBSTYPE OF T, THEN OBJECT OF THE TYPE T IN A PROGRAM MAY BE REPLACED WITH OBJECT OF TYPE S WITHOUT ALTWERING ANY OF THE DESIRABLE PROPERTIES OF THAT PROGRAM)
+
+// abstract class Shape {
+//   abstract calculateArea(): number;
+// }
+
+// class Rectangle extends Shape {
+//   constructor(
+//     public width: number,
+//     public height: number,
+//   ) {
+//     super();
+//   }
+//   public calculateArea(): number {
+//     return this.width * this.height;
+//   }
+// }
+
+// class Square extends Shape {
+//   constructor(public width: number) {
+//     super();
+//   }
+//   public calculateArea(): number {
+//     return this.width * this.width;
+//   }
+// }
+
+// function area(shape: Shape) {
+//   //This is expecting SHAPE a parent class
+//   return shape.calculateArea();
+// }
+
+// const rect = new Rectangle(4, 5);
+// const squa = new Square(5);
+
+// area(rect); //but we are using it with a child class instead...LISKOV
+
+//IMPLEMENTATION
+// CREATE A PAYMENT PROCESSOR
+// PROCESS CREDIT CARD
+//  /DEBIT CARD
+// PAYPAL
+abstract class Payment {
+  abstract debitCustomer(): number;
+}
+
+class CreditCard extends Payment {
+  constructor(public amount: number) {
+    super();
+  }
+  debitCustomer(): number {
+    let tax = 10;
+    return this.amount + tax;
+  }
+}
+
+class DebitCard extends Payment {
+  constructor(public amount: number) {
+    super();
+  }
+  debitCustomer(): number {
+    let tax = 20;
+    return this.amount + tax;
+  }
+}
+
+class PayPal extends Payment {
+  constructor(public amount: number) {
+    super();
+  }
+  debitCustomer(): number {
+    let tax = 12;
+    return this.amount + tax;
+  }
+}
+
+function makePayment(paymentMethod: Payment) {
+  return paymentMethod.debitCustomer();
+}
+
+const newCustomer = new CreditCard(2000);
+
+console.log(makePayment(newCustomer));
+
+//L Closed
 
 // WRONG WAY OF CREATING A DISCOUNT CALCULATOR CUS WHAT IF I WANT TO ADD MORE CUSTOMER TYPES
 // TYPES I HAVE RN
@@ -18,57 +103,58 @@
 //     }
 // }
 
-//RIGHT NOW
-interface Customer {
-  giveDiscount(): number;
-  addLoyaltyPoints(amountSpent: number): number;
-}
+//RIGHT NOW OPEN-CLOSED
+// interface Customer {
+//   giveDiscount(): number;
+//   addLoyaltyPoints(amountSpent: number): number;
+// }
 
-class RegularCustomer implements Customer {
-  constructor() {}
-  giveDiscount(): number {
-    console.log("10");
+// class RegularCustomer implements Customer {
+//   giveDiscount(): number {
+//     console.log("10");
 
-    return 10;
-  }
-  addLoyaltyPoints(amountSpent: number): number {
-    return amountSpent;
-  }
-}
+//     return 10;
+//   }
+//   addLoyaltyPoints(amountSpent: number): number {
+//     return amountSpent;
+//   }
+// }
 
-class PremiumCustomer implements Customer {
-  giveDiscount(): number {
-    console.log("20");
-    return 20;
-  }
+// class PremiumCustomer implements Customer {
+//   giveDiscount(): number {
+//     console.log("20");
+//     return 20;
+//   }
 
-  addLoyaltyPoints(amountSpent: number): number {
-    return amountSpent * 1.4;
-  }
-}
+//   addLoyaltyPoints(amountSpent: number): number {
+//     return amountSpent * 1.4;
+//   }
+// }
 
-class GoldCustomer implements Customer {
-  giveDiscount(): number {
-    console.log("50");
-    return 500;
-  }
-  addLoyaltyPoints(amountSpent: number): number {
-    return amountSpent * 2;
-  }
-}
+// class GoldCustomer implements Customer {
+//   giveDiscount(): number {
+//     console.log("50");
+//     return 500;
+//   }
+//   addLoyaltyPoints(amountSpent: number): number {
+//     return amountSpent * 2;
+//   }
+// }
 
-class DiscountOuter {
-  giveDiscount(customer: Customer) {
-    return customer.giveDiscount();
-  }
-  addLoyaltyPoints(amountSpent: number): number {
-    return amountSpent * 3;
-  }
-}
+// class DiscountOuter {
+//   giveDiscount(customer: Customer) {
+//     return customer.giveDiscount();
+//   }
+//   addLoyaltyPoints(amountSpent: number): number {
+//     return amountSpent * 3;
+//   }
+// }
 
-const Fas: PremiumCustomer = new PremiumCustomer();
-const calcDiscount: DiscountOuter = new DiscountOuter();
-calcDiscount.giveDiscount(Fas);
+// const Fas: PremiumCustomer = new PremiumCustomer();
+// const calcDiscount: DiscountOuter = new DiscountOuter();
+// calcDiscount.giveDiscount(Fas);
+
+// OPEN-CLOSED ABOVE
 
 //S IS BELOW
 // class BlogPost {
